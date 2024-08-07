4b2d6329708f07edb28df64ab1b048c12cbfc7c8
Matheus P Mufato <mmufato@gmail.com>
2024-08-07 20:12:11 -0300

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,16 @@
+import { Html } from '@react-three/drei'
+
+const Loader = () => {
+    return (
+        <Html>
+        <div className="absolute top-o left-0 w-full h-full
+            flex justify-center items-center">
+                <div className="w-[10vh] h-[10vh] rounded-full">
+                    Loading...
+                </div>
+            </div>
+        </Html>
+    )
+}
+
+export default Loader
